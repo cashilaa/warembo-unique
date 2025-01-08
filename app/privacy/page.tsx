43c125a0e47a91cfd9b76b1380c2516bda53c5eb
smartgreendeer <johnmuriithi7818@gmail.com>
2025-01-08 08:57:29 +0300

    Fix Privacy page component export

---
+++ app/privacy/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrivacyPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>Your privacy policy content goes here.</p>
+        </div>
+    );
+};
+
+export default PrivacyPage;
